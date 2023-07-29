165805983d2bca573c716f869b619b68735a3cf7
AJ-Kulundu <jameskulundu@gmail.com>
2023-07-29 16:35:22 +0300

    Editing devise path and creating community resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "home#index"
+  resources :communities 
 end
