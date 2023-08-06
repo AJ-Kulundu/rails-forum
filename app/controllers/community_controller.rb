53edf6974b3bbb1a4856955903ed1de68a5b33e4
AJ-Kulundu <jameskulundu@gmail.com>
2023-08-06 11:13:10 +0300

    Creating the community controller

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,2 +1,25 @@
 class CommunityController < ApplicationController
+    skip_before_action :authenticate_user!, only: %i[index show]
+    before_action :set_community, only: %i[show edit update destroy]
+
+    def index
+        @communities = Community.all
+    end
+
+    def show; end
+
+    def new
+        @community = Community.new
+    end
+
+    private
+
+    def community_params
+        params.require(:community).permit(:name, :description)
+    end
+
+    def set_community
+        @community = Community.find(params[:id])
+    end
+
 end
