e3b15eb40b69ce36a0fa7e3c60270a67357cbc5d
AJ-Kulundu <jameskulundu@gmail.com>
2023-08-19 13:37:07 +0300

    Adding tailwind custom colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    colors:{
+      primary: "#a78bfa",
+      secondary: "#a4cbb4",               
+      accent: "#dc8850",               
+      neutral: "#2e282a",               
+      baseColor: "#e4d8b4",               
+      info: "#2463eb",               
+      success: "#16a249",               
+      warning: "#db7706",               
+      error: "#dc2828",
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
