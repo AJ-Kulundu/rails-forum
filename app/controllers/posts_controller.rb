8867345e6e18e32e5522d6949984a1cfd8f3e939
AJ-Kulundu <jameskulundu@gmail.com>
2023-08-21 21:39:08 +0300

    Cretaing posts feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+    before_action :set_community
+    before_action :set_post, only: %i[show edit update destroy]
+    def index
+        @posts = Post.all.community(@community.id).ordered
+    end
+
+    def new
+        @post = @community.posts.build
+    end
+
+    def create
+        @post = @community.posts.build(post_params)
+
+        if @post.save
+            redirect_to community_path(@community)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+         def post_params
+            params.require(:post).permit(:body).merge(user: current_user)
+        end
+
+        def set_community
+            @community = Community.find(params[:community_id])
+        end
+
+        def set_post
+            @post = Post.find(params[:id])
+        end
 end
