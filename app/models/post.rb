3f4f7016206fee8af3256825862a277243958f66
AJ-Kulundu <jameskulundu@gmail.com>
2023-08-21 21:39:34 +0300

    Adding post model scopes and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :community
+
+  validates :body, presence: true, length: { maximum: 200 }
+  validates :user_id, presence: true
+  validates :community_id, presence: true
+
+  scope :community, ->(id) { where(community_id: id) }
+  scope :user, ->(id) { where(user_id: id) }
+  scope :ordered, -> { order(created_at: :desc) }
 end
